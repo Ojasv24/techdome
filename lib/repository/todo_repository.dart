27c1ae161e48b8b8f87938e2dd06869d97c60a8d
Ojasv <ojasv.rathore@gmail.com>
2023-04-28 17:33:53 +0530

    Todo repository  completed

---
+++ lib/repository/todo_repository.dart
@@ -0,0 +1,46 @@
+import 'dart:async';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:techdome/model/todo.dart';
+
+part 'todo_repository.g.dart';
+
+@riverpod
+TodoRepository todoRepository(TodoRepositoryRef ref) {
+  return TodoRepository();
+}
+
+class TodoRepository {
+  String get uid {
+    return FirebaseAuth.instance.currentUser!.uid;
+  }
+
+  CollectionReference get collection {
+    return FirebaseFirestore.instance.collection(uid);
+  }
+
+  Future<void> create(String text) async {
+    final doc = collection.doc();
+    final todo = Todo(id: doc.id, description: text, completed: false);
+    await doc.set(todo.toJson());
+  }
+
+  Future<void> update(Todo todo) async {
+    await collection.doc(todo.id).update(todo.toJson());
+  }
+
+  Future<void> delete(Todo todo) async {
+    await collection.doc(todo.id).delete();
+  }
+
+  Stream listenTodoList() {
+    // listen todoModel to firestore user
+    return collection.snapshots().map((snapshot) {
+      return snapshot.docs.map((doc) {
+        return Todo.fromJson(doc.data() as Map<String, dynamic>);
+      }).toList();
+    });
+  }
+}
---
+++ lib/repository/todo_repository.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'todo_repository.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$todoRepositoryHash() => r'bd082c7811065305e849f34378ad710cd0d17f50';
+
+/// See also [todoRepository].
+@ProviderFor(todoRepository)
+final todoRepositoryProvider = AutoDisposeProvider<TodoRepository>.internal(
+  todoRepository,
+  name: r'todoRepositoryProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$todoRepositoryHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef TodoRepositoryRef = AutoDisposeProviderRef<TodoRepository>;
+// ignore_for_file: unnecessary_raw_strings, subtype_of_sealed_class, invalid_use_of_internal_member, do_not_use_environment, prefer_const_constructors, public_member_api_docs, avoid_private_typedef_functions
